c53619cecab94cceee9cff9acc8f80e286c87471
Himanshu-Finkhoz <himanshu.p@finkhoz.com>
2025-08-18 16:48:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,12 +5,12 @@ import pandas as pd
 
 def download_csv_FO():
     today = datetime.date.today()
-    date = today - datetime.timedelta(days=4)  # yesterday
-    formatted_date = date.strftime("%Y%m%d")   # YYYYMMDD format
+    date = today - datetime.timedelta(days=4)  
+    formatted_date = date.strftime("%Y%m%d")   
 
     url = f"https://www.bseindia.com/download/BhavCopy/Derivative/BhavCopy_BSE_FO_0_0_0_{formatted_date}_F_0000.CSV"
 
-    # add headers to mimic a browser
+    
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                       "AppleWebKit/537.36 (KHTML, like Gecko) "
@@ -25,10 +25,10 @@ def download_csv_FO():
         filename = f"bhavcopy_{formatted_date}.csv"
         with open(filename, "wb") as f:
             f.write(r.content)
-        print(f"✅ Downloaded: {filename}")
+        print(f"Downloaded: {filename}")
         return filename
     else:
-        print(f"❌ Download failed (HTTP {r.status_code}) — possibly holiday or no trading.")
+        print(f" Download failed (HTTP {r.status_code}) — possibly holiday or no trading.")
         return None
 
 filename = download_csv_FO()
